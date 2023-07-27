a0d30ef8f7a22d15a05aba1c52f9ff84f839b863
Bryan Travis Smith <bryantravissmith@gmail.com>
2023-07-26 19:07:02 -0700

    saved figured

--- training/train_model.py
+++ training/train_model.py
@@ -5,7 +5,8 @@ import pandas as pd
 import os
 import json
 from joblib import dump
-from typing import Dict, Tuple
+import matplotlib.pyplot as plt
+from typing import Dict, List, Tuple
 import yaml
 from yaml import CLoader as Loader
 from sklearn.ensemble import RandomForestClassifier
@@ -15,18 +16,20 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import (
-    f1_score, recall_score, precision_score, accuracy_score, roc_auc_score
+    f1_score, recall_score, precision_score, accuracy_score
 )
 
 
 def train(params: Dict):
     X, y = load_data(params['preproccess_output_path'])
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+                                                        random_state=42)
     categorical_features = sorted(
         params['random_forest']['features']['categorical']
     )
     categorical_transformer = make_pipeline(
-        SimpleImputer(strategy="constant", fill_value=''), OrdinalEncoder()
+        SimpleImputer(strategy="constant", fill_value=''),
+        OrdinalEncoder(min_frequency=300),
     )
 
     preprocessor = ColumnTransformer(
@@ -48,7 +51,16 @@ def train(params: Dict):
     )
     pipe.fit(X_train, y_train)
 
+    y_pred = pipe.predict(X_test)
+    X_test.assign(
+        target=y_test,
+        pred=y_pred
+    ).pipe(
+        save_f1_plots
+    )
+
     save_model_scores(pipe, X_test, y_test)
+
     dump(
         pipe,
         os.path.join(
@@ -64,26 +76,38 @@ def calculate_scores(y: pd.Series, y_pred: pd.Series) -> Tuple[
     precision = precision_score(y_pred, y)
     recall = recall_score(y_pred, y)
     accuracy = accuracy_score(y_pred, y)
-    auc = roc_auc_score(y_pred, y)
-    return f1, precision, recall, accuracy, auc
+    return f1, precision, recall, accuracy
+
+
+def save_f1_plots(
+        dataframe: pd.DataFrame,
+        prediction: str = 'pred',
+        target: str = 'target',
+        categories: List[str] = ['relationship', 'sex', 'race', 'occupation']
+):
+    for cat in categories:
+        plt.clf()
+        dataframe.groupby(cat).apply(
+            lambda x: groupby_scores(x, prediction, target)
+        ).f1.sort_values().plot(kind='bar')
+        plt.savefig(os.path.join('plots', f"{cat}.png"))
 
 
 def groupby_scores(row: pd.Series, target: str = 'y',
                    prediction: str = 'prediciton') -> pd.Series:
     result = {}
-    f1, precision, recall, accuracy, auc = calculate_scores(
+    f1, precision, recall, accuracy = calculate_scores(
         row[target], row[prediction])
     result['f1'] = f1
     result['precision'] = precision
     result['recall'] = recall
     result['accuracy'] = accuracy
-    result['auc'] = auc
     return pd.Series(result)
 
 
 def save_model_scores(model: Pipeline, X: pd.DataFrame, y: pd.Series):
     y_pred = model.predict(X)
-    f1, precision, recall, accuracy, auc = calculate_scores(y, y_pred)
+    f1, precision, recall, accuracy = calculate_scores(y, y_pred)
 
     with open("model/scores.json", "w") as f:
         json.dump({
@@ -91,7 +115,6 @@ def save_model_scores(model: Pipeline, X: pd.DataFrame, y: pd.Series):
             "precision": precision,
             "recall": recall,
             "accuracy": accuracy,
-            "auc": auc,
         }, f)
 
 
